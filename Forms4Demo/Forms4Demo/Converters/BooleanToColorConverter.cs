762658b53976e345b34fdab2f832f18e38bed7fb
jesulink2514 <jesus.angulo@outlook.com>
2019-05-26 17:19:35 -0500

    Add project files.

---
+++ Forms4Demo/Forms4Demo/Converters/BooleanToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Forms4Demo.Converters
+{
+    public class BooleanToColorConverter : IValueConverter
+    {
+        public Color TrueColor { get; set; }
+        public Color FalseColor { get; set; }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isBool = (bool)value;
+            return isBool ? TrueColor : FalseColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Forms4Demo/Forms4Demo/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Forms4Demo.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Forms4Demo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();            
+        }
+    }
+}
